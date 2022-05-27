7f2122705bd43ec8bf1fb54406cb8348659d5b1c
PrairieWaltz <85768337+PrairieWaltz@users.noreply.github.com>
2022-05-27 09:41:30 -0400

    Home Page and List overhaul

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+// REQUIREMENTS //
+
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
 const methodOverride = require("method-override");
 const Favorite = require("./models/favorite");
+
+// MONGODB ROUTE //
 
 mongoose.connect("mongodb://localhost:27017/cahier", {
   useNewUrlParser: true,
@@ -15,16 +20,33 @@ db.once("open", () => {
   console.log("Database connected");
 });
 
+// EXPRESS VIEW PATHS //
+
 const app = express();
 
+app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
+// GET ROUTES //
+
 app.get("/", (req, res) => {
   res.render("home");
+});
+
+app.get("/sign-in/signin", (req, res) => {
+  res.render("sign-in/signin");
+});
+
+app.get("/sign-in/signup", (req, res) => {
+  res.render("sign-in/signup");
+});
+
+app.get("/sign-in/howitworks", (req, res) => {
+  res.render("sign-in/howitworks");
 });
 
 app.get("/favorites", async (req, res) => {
@@ -52,6 +74,8 @@ app.get("/favorites/:id/edit", async (req, res) => {
   res.render("favorites/edit", { favorite });
 });
 
+// UPDATE PAGE ROUTES //
+
 app.put("/favorites/:id", async (req, res) => {
   const { id } = req.params;
   const favorite = await Favorite.findByIdAndUpdate(id, {
@@ -60,11 +84,15 @@ app.put("/favorites/:id", async (req, res) => {
   res.redirect(`/favorites/${favorite._id}`);
 });
 
+// DELETE PAGE ROUTES //
+
 app.delete("/favorites/:id", async (req, res) => {
   const { id } = req.params;
   await Favorite.findByIdAndDelete(id);
   res.redirect("/favorites");
 });
+
+// SERVER FUNCTION //
 
 app.listen(3000, () => {
   console.log("Loud and Clear on Port 3000");
--- models/favorite.js
+++ models/favorite.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema;
 
 const FavoriteSchema = new Schema({
   name: String,
+  image: String,
   description: String,
   price: Number,
   rating: String,
